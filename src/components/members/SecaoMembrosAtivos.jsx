42e075eec2e64fe143a7146bbf6b24971161ff93
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-06-30 06:47:48 -0300

    feat: secao de membros

--- src/components/members/SecaoMembrosAtivos.jsx
+++ src/components/members/SecaoMembrosAtivos.jsx
@@ -3,27 +3,131 @@ import "../../styles/members/SecaoMembrosAtivos.css"
 export default function SecaoMembrosAtivos() {
     return (
         <section className="secao-membros-ativos">
-            <div class="coluna">
-                <p>Membros Ativos</p>
-                <div class="galeria">
-                    <div class="item-galeria">
-                        <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} alt="Descrição 1" />
-                        <span>Descrição</span>
-                    </div>
-                    <div class="item-galeria">
-                        <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} alt="Descrição 2" />
-                        <span>Descrição</span>
-                    </div>
-                    <div class="item-galeria">
-                        <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} alt="Descrição 3" />
-                        <span>Descrição</span>
-                    </div>
-                    <div class="item-galeria">
-                        <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} alt="Descrição 4" />
-                        <span>Descrição</span>
+            <div className="topo">
+                <p>Membros</p>
+            </div>
+            <div class="galeria">
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
                 </div>
-        </div>
-   </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+                <div class="item-galeria">
+                    <img src={process.env.PUBLIC_URL + "/images/avatar.avif"} 
+                            alt="Descrição 1" 
+                            className="foto-membro-ativo" />
+                    <p className="nome">Nome</p>
+                </div>
+            </div>
         </section>
     );
 }
