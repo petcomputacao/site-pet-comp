4e2e3f39c1afdd5157246091d61421c291cf91f4
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-07-06 12:57:53 -0300

    fix: alterando identificador para className

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 
-import Aurora     from './components/pagina-inicial/Aurora';
-import LogoPET   from './components/header-static/LogoPET';
-import Navbar     from './components/header-static/Navbar';
-import Footer     from './components/Footer';
-import TextoPET   from './components/pagina-inicial/TextoPET';
-import SecaoTutor         from './components/membros/SecaoTutor';
-import SecaoMembrosAtivos from './components/membros/SecaoMembrosAtivos';
-import SecaoSobre         from './components/sobre/SecaoSobre';
-import AutoPlay from './components/projectspage/InfiteAutoplay';
-import TextoProjects from './components/projectspage/TextoProjects';
-import SliderImages from './components/pagina-inicial/Slider';
+import Aurora               from './components/pagina-inicial/Aurora';
+import LogoPET              from './components/header-static/LogoPET';
+import Navbar               from './components/header-static/Navbar';
+import Footer               from './components/Footer';
+import TextoPET             from './components/pagina-inicial/TextoPET';
+import SecaoTutor           from './components/membros/SecaoTutor';
+import SecaoMembrosAtivos   from './components/membros/SecaoMembrosAtivos';
+import SecaoSobre           from './components/sobre/SecaoSobre';
+import AutoPlay             from './components/projectspage/InfiteAutoplay';
+import TextoProjects        from './components/projectspage/TextoProjects';
+import SliderImages         from './components/pagina-inicial/Slider';
 import LogoPETpaginaInicial from './components/pagina-inicial/LogoPETpaginaInicial';
+
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
 
       {/* Sessão das "partes" da pagina */}
       <main>
-        {/* Pagina Inicial */}
+          {/* Pagina Inicial */}
           <section id="paginaInicial" className="paginaInicial">
             <div className="content-wrapper"> 
               <section className='esquerda-pagina-inicial'>
@@ -42,43 +43,41 @@ function App() {
             <Aurora/>
           </section>
 
-        {/* Sobre */}
+          {/* Sobre */}
           <section id="Sobre" className="Sobre">
             <SecaoSobre />
           </section>
 
-        {/* Projetos */}
-        <div className='blockpage'>
-          <section id="Projetos" className="Projetos">
-            <div className='textosProjects'>
-              <TextoProjects />
-            </div>
-            <div className="content-projects"> 
-              <AutoPlay/>
-            </div>
-          </section>
-        </div>
+          {/* Projetos */}
+          <div className='blockpage'>
+            <section id="Projetos" className="Projetos">
+              <div className='textosProjects'>
+                <TextoProjects />
+              </div>
+              <div className="content-projects"> 
+                <AutoPlay/>
+              </div>
+            </section>
+          </div>
 
-        {/* Pagina dedicada ao Tutor(a) */}
-        <div className='blockpage'>
-          <section id="Tutor" className="PaginaTutor">
-            <SecaoTutor />
-          </section>
-        </div>
+          {/* Pagina dedicada ao Tutor(a) */}
+          <div className='blockpage'>
+            <section id="Tutor" className="PaginaTutor">
+              <SecaoTutor />
+            </section>
+          </div>
 
-        {/* Pagina dedicada aos membros ativos */}
-        <div className='blockpage'>
-        <section id="Membros" className ="PaginaMembros">
-          <SecaoMembrosAtivos />
-        </section>
-        </div>
-        
-    </main>
-    {/* Rodapé da pagina */}
-    <footer>
-      <Footer />
-    </footer>
-
+          {/* Pagina dedicada aos membros ativos */}
+          <div className='blockpage'>
+            <section id="Membros" className ="PaginaMembros">
+              <SecaoMembrosAtivos />
+            </section>
+          </div>
+      </main>
+      {/* Rodapé da pagina */}
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
