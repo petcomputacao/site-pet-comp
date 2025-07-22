6463d2376383d88dfa82e438fd0df2193ef773e8
Arthur Fernandes Vieria <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-07-22 06:31:01 -0300

    feat: novo in progress projects

---
+++ src/components/static/CardCarrousel.jsx
@@ -0,0 +1,21 @@
+import "./CardCarrousel.css";
+
+
+export default function CardCarrousel() {
+    return (
+        <div className="container__carrousel">
+            <div className="carrousel__card">
+                Post 1
+            </div>
+            <div className="carrousel__card">
+                Post 2
+            </div>
+            <div className="carrousel__card">
+                Post 3
+            </div>
+            <div className="carrousel__card">
+                Post 4
+            </div>
+        </div>
+    )
+}
