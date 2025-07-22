1116366757db491f72f46d3a53ac8bc28cc8e344
Arthur Fernandes Vieria <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-07-22 14:18:15 -0300

    feat: adicionando animações na secao de membros

--- src/components/pages/About/SecaoSobre.jsx
+++ src/components/pages/About/SecaoSobre.jsx
@@ -11,24 +11,31 @@ export default function SecaoSobre() {
                     <h2 className="pesquisa__titulo">
                         PESQUISA
                     </h2>
+                    <Reveal>
                     <p className="pesquisa__texto">
                         Todos os integrantes do PET Computação desenvolvem pesquisas em diversas áreas da computação, produzem artigos científicos e buscam soluções que fortalecem o conhecimento e a inovação dentro do ambiente acadêmico.                    </p>
+                    </Reveal>
                 </div>
+
                 <div className="pilares__ensino">
                     <img src={process.env.PUBLIC_URL + "/images/logos/logo_ensino.png"} className="ensino-logo" alt="logo_ensino" />
                     <h2 className="ensino__titulo">
-                        ENSINO
+                        ENSINO    
                     </h2>
+                    <Reveal>
                     <p className="ensino__texto">
                         Os integrantes do PET Computação participam ativamente da vida acadêmica, promovendo eventos e atividades que estimulam o aprendizado, a troca de conhecimento e o desenvolvimento dos alunos da graduação.                    </p>
+                    </Reveal>
                 </div>
                 <div className="pilares__tutoria">
                     <img src={process.env.PUBLIC_URL + "/images/logos/logo_tutoria.png"} className="tutoria-logo" alt="logo_tutoria" />
                     <h2 className="tutoria__titulo">
                         EXTENSÃO
                     </h2>
+                <Reveal>
                     <p className="tutoria__texto">
                         O PET Computação atua em Campina Grande promovendo inclusão digital e tecnologia por meio de oficinas, cursos e projetos sociais que buscam ampliar o acesso ao conhecimento e melhorar a qualidade de vida da comunidade.                    </p>
+                </Reveal>
                 </div>
             </div>
         </div>
