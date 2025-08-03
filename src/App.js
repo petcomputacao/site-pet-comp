8e4894950e32b3058b619c7e362ac5cffbf38b3a
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-08-03 19:42:38 -0300

    fix: conserta incoerencias com ultimo merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 
 import Header from './components/static/Header/Header';
-import SecaoSobre from './components/pages/About/SecaoSobre';
-import Producoes from './components/pages/InProgressProjects/Producoes';
-import MembrosAtivos from './components/pages/MemberPage/MembrosAtivosPage/MembrosAtivos';
-import Footer from './components/static/Footer/Footer';
 import HomePet from './components/pages/HomePage/HomePET';
 import Vertentes from './components/pages/ProjectsLines/VertentesProjects';
 import SecaoTutor from './components/pages/MemberPage/TutorPage/SecaoTutor';
